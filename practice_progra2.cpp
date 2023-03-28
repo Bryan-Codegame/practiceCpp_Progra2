b7cdc8dc66d912493e7f850816a3c47a9336ed63
Bryan-CodeGame <bryanesc26@gmail.com>
2023-03-28 09:08:30 -0600

    Vectors

--- practice_progra2.cpp
+++ practice_progra2.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int askNumber(string question, int high, int low = 1);
+//GUESS MY NUMBER
+void guessMyNumber();
+
+
 
 int main()
+{
+    //vector<string> myStuff = { "espada", "martillo", "bomba" };
+    //vector<string> inventory(10);
+    //vector<string> inventory(10, " Vacio ");
+    //vector<string> inventory(myStuff);
+
+    vector<string> inventory;
+
+    inventory.push_back("Espada");
+    inventory.push_back("Escudo");
+    inventory.push_back("Martillo");
+
+    cout << "Tienes " << inventory.size() << " items en tu inventario" << endl;
+
+    //Iteration FOR
+    for (unsigned int i = 0; i < inventory.size(); i++)
+    {
+        cout << inventory[i] << endl;
+    }
+
+    cout << "\nCambiaste tu " << inventory[0] << " por una Bomba.";
+    inventory[0] = "Bomba";
+
+    cout << "\n";
+
+    //Iteration DO-WHILE
+    int i = 0;
+    do {
+        cout << inventory[i] << endl;
+        i++;
+    } while (i < inventory.size());
+}
+
+
+
+int askNumber(string question, int high, int low)
+{
+    int number = 0;
+
+    do {
+        cout << question << "entre " << low << " y " << high << endl;
+        cin >> number;
+    } while (number > high || number < low);
+
+    return number;
+}
+
+
+void guessMyNumber()
 {
     srand(static_cast<unsigned int>(time(0)));
     //srand(time(NULL));
@@ -15,7 +69,7 @@ int main()
     int tries = 0;
     int veryClose;
 
-    
+
     cout << "GUESS MY NUMBER" << endl;
     cout << "Adivina el número en el menor número de intentos posible." << endl;
 
@@ -48,17 +102,3 @@ int main()
         }
     } while (guess != secretNumber);
 }
-
-int askNumber(string question, int high, int low)
-{
-    int number = 0;
-
-    do {
-        cout << question << "entre " << low << " y " << high << endl;
-        cin >> number;
-    } while (number > high || number < low);
-
-    return number;
-}
-
-
